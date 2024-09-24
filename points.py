19089694dcda69718ec9fa7201921a67a0d76de6
marie <theo.attonaty@gmail.com>
2024-09-24 15:07:54 +0200

    Merge remote-tracking branch 'origin/ajout-des-points' into ajout-des-points

--- points.py
+++ points.py
@@ -1,11 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
+from matplotlib.animation import FuncAnimation
 
 class objet:
     def __init__(self, n):
         self.n = n
+
+    def calc_points(self,  t):
+        pass
+
+    def distance_radar(self):
+        return np.sqrt(np.sum((self.points - self.radar @ self.ones)**2, axis=1))
+
+    def empl_points(self, num, pas, scatter):
+        """
+        C'est cette fonction qui est appelé lors de la visualisation.
+        :param num: Le numero de la frame a afficher, necessaire au fonctionnement de Funcanimation
+        :param pas: Necessaire pour convertir l'incrementation de 1 en 1 en un temps.
+        :return: l'emplacement des points de l'objet à l'instant, t = num*pas
+        """
+        self.calc_points(num*pas)
+        scatter._offsets3d = self.centre @ self.ones  + self.points
+        return self.points
+    def vitesse(self, t, delta_t):
+        """
+        C'est cette fonction qui est appelé lorsqu'on va vouloir tracer le spectrogramme
+        :param
+        :param t:
+        :param delta_t:
+        :return: Un tableau de une ligne et self.n colonnes, chaque colonne indique la vitesse du ième point par rapport au radar
+        """
+        self.calc_points(t)
+        distances = self.distance_radar()
+        a = (distances - self.distance_anc)/delta_t
+        self.distance_anc = distances
+        return a
 
 
 class objet_fixe(objet):
@@ -28,45 +58,31 @@ class objet_rotation(objet):
         :param u: Le vecteur qui définit le sens de rotation
         """
         super().__init__(n)
-        self.points = np.array(points)
-        self.centre = np.array(centre)
+
+        self.radar = np.atleast_2d(radar).T
+        self.points_init = np.array(points).T
+        print(self.points_init.shape)
+        self.points = self.points_init.copy()
+        self.centre = np.atleast_2d(centre).T
         self.ones = np.ones((1,n))
         self.v, self.u =v, u
-        self.distance_anc = self.distance_radar(radar)
+        self.distance_anc = self.distance_radar()
     def calcul_rot(self, theta):
         u = self.u
         c = np.cos(theta)
         s = np.sin(theta)
         self.rot = np.array([[u[0]**2*(1-c)+c, u[0]*u[1]*(1-c)-u[2]*s, u[0]*u[2]*(1-c)+u[1]*s],
-                             [u[0]*u[1]*(1-c) + u[1]**2*(1-c)+c, u[1]*u[2]*(1-c) - u[0]*s],
+                             [u[0]*u[1]*(1-c) + u[2]*s, u[1]**2*(1-c)+c, u[1]*u[2]*(1-c) - u[0]*s],
                              [u[0]*u[2]*(1-c)-u[1]*s, u[1]*u[2]*(1-c)+u[0]*s, u[2]**2*(1-c)+c]])
 
 
     def calc_points(self, t):
         self.calcul_rot(self.v*t)
-        self.points = self.centre @ self.ones + self.rot @ self.points
-
-    def distance_radar(self, radar):
-        return np.sqrt(np.sum((self.points - self.radar @ self.ones)**2, axis=1))
-    def vitesse(self, t, delta_t, radar):
-        """
-        :param
-        :param t:
-        :param delta_t:
-        :return: Un tableau de une ligne et self.n colonnes, chaque colonne indique la vitesse du ième point par rapport au radar
-        """
-        self.calc_points(t)
-        distances = self.distance_radar(radar)
-        a = (distances - self.distance_anc)/delta_t
-        self.distance_anc = distances
-        return a
+        self.points = self.centre @ self.ones + self.rot @ self.points_init
 
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
+
 
 def plot_3d_points(x, y, z):
     fig = plt.figure()
@@ -82,17 +98,36 @@ def plot_3d_points(x, y, z):
         y = y + 0.1 * num
         z = z + 0.1 * num
         scatter._offsets3d = (x, y, z)
-        return scatter,
+        tab = np.array([x,y,z])
+        return x, y, z
 
     ani = FuncAnimation(fig, update, frames=100, fargs=(x, y, z, scatter), interval=100, blit=False)
     plt.show()
 
-# Exemple d'utilisation
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([5, 6, 2, 3, 13])
 z = np.array([2, 3, 3, 3, 5])
 
-plot_3d_points(x, y, z)
+# plot_3d_points(x, y, z)
+
+def affiche_objet(ob : objet, temps_anim, pas):
+    frames = int(temps_anim // pas)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(ob.points[:, 0], ob.points[:, 1], ob.points[:, 2], c='r', marker='o')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    ani = FuncAnimation(fig, ob.empl_points, frames = frames, fargs=[pas, scatter], interval=pas*1000, blit = False)
+    plt.show()
 
 
+pt_init =           [[1, 1, 0],
+                     [-1, 1, 0],
+                     [-1, -1, 0],
+                     [1, -1, 0]]
 
+rotation = objet_rotation(4, pt_init, (0,0,0),2, (0,0,1), (-15, 0, 0))
+
+
+affiche_objet(rotation, 10, 0.1)
